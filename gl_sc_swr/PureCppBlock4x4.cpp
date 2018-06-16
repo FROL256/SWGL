1d14de60343c9f7c7aa08b7b98889859855bb81d
FROL-HOME\FROL <vova@frolov.pp.ru>
2018-06-16 19:35:46 +0300

    simple block algorithm

--- gl_sc_swr/PureCppBlock4x4.cpp
+++ gl_sc_swr/PureCppBlock4x4.cpp
@@ -9,15 +9,10 @@
 
 using TriangleLocal = HWImplementationPureCpp::TriangleType;
 
-#define QSIZE 4
 
-void RasterizeTriHalfSpace2D(const TriangleLocal& tri, int tileMinX, int tileMinY,
-                             FrameBuffer* frameBuf)
+void RasterizeTriHalfSpace2D_Block(const TriangleLocal& tri, int tileMinX, int tileMinY,
+                                   FrameBuffer* frameBuf)
 {
-  // already done (triangle set up): Calculate triangle bounding box(minX, minY, maxX, maxY);
-  // already done (triangle set up): Clip box against render target bounds(minX, minY, maxX, maxY);
-  //
-  
   const float tileMinX_f = float(tileMinX);
   const float tileMinY_f = float(tileMinY);
 
@@ -57,49 +52,41 @@ void RasterizeTriHalfSpace2D(const TriangleLocal& tri, int tileMinX, int tileMin
   float Cy2 = C2 + Dx23 * miny - Dy23 * minx;
   float Cy3 = C3 + Dx31 * miny - Dy31 * minx;
 
-  //int offset = lineOffset(miny, frameBuf->w, frameBuf->h);
+  int offset = lineOffset(miny, frameBuf->w, frameBuf->h);
+  
+  constexpr int   blockSize  = 4;
+  constexpr float blockSizeF = float(blockSize);
 
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-  const int halfY     = tri.bb_iminY + ((tri.bb_imaxY - tri.bb_iminY) >> 1) & ~(QSIZE - 1);
-  const int BlockSize = QSIZE;
-
-  // Find Topmost vertex, calculate leftPoint and rightPoint ... (#NOT_SURE !!!)
-  int leftPoint  = tri.bb_iminX;
-  int rightPoint = tri.bb_imaxX;
-
-  //Loop j=minY to (halfY + BlockSize) step=BlockSize
-  for (int j = tri.bb_iminY; j < halfY + BlockSize; j += BlockSize)
+  // Scan through bounding rectangle
+  for (int by = miny; by <= maxy; by += blockSize)
   {
-    const int midPoint = leftPoint + ((rightPoint - leftPoint) >> 1) & ~(BlockSize - 1);
+    // Start value for horizontal scan
+    float Cx1 = Cy1;
+    float Cx2 = Cy2;
+    float Cx3 = Cy3;
 
-    //Loop k=0 to 2 step=1
-    //Loop x to (q > 0 ? x < maxX : x > minX - BlockSize) step=q
-    for (int x = midPoint; x < tri.bb_imaxX; x += BlockSize) // k = 0
+    for (int bx = minx; bx <= maxx; bx+= blockSize)
     {
-      const float fx1 = float(x);
-      const float fy1 = float(j);
-      const float fx2 = float(x + BlockSize - 1);
-      const float fy2 = float(j + BlockSize - 1);
+      const float fx1 = bx;
+      const float fy1 = by;
+      const float fx2 = bx + blockSizeF;
+      const float fy2 = by + blockSizeF;
 
-      float Cx1_00 = C1 + Dx12 * fy1 - Dy12 * fx1;
-      float Cx2_00 = C2 + Dx23 * fy1 - Dy23 * fx1;
-      float Cx3_00 = C3 + Dx31 * fy1 - Dy31 * fx1;
-       
-      float Cx1_01 = C1 + Dx12 * fy1 - Dy12 * fx2;
-      float Cx2_01 = C2 + Dx23 * fy1 - Dy23 * fx2;
-      float Cx3_01 = C3 + Dx31 * fy1 - Dy31 * fx2;
+      const float Cx1_00 = Cx1;
+      const float Cx2_00 = Cx2;
+      const float Cx3_00 = Cx3;
 
-      float Cx1_10 = C1 + Dx12 * fy2 - Dy12 * fx1;
-      float Cx2_10 = C2 + Dx23 * fy2 - Dy23 * fx1;
-      float Cx3_10 = C3 + Dx31 * fy2 - Dy31 * fx1;
+      const float Cx1_01 = Cx1 - Dy12*blockSizeF;
+      const float Cx2_01 = Cx2 - Dy23*blockSizeF;
+      const float Cx3_01 = Cx3 - Dy31*blockSizeF;
 
-      float Cx1_11 = C1 + Dx12 * fy2 - Dy12 * fx2;
-      float Cx2_11 = C2 + Dx23 * fy2 - Dy23 * fx2;
-      float Cx3_11 = C3 + Dx31 * fy2 - Dy31 * fx2;
+      const float Cx1_10 = Cx1 + Dx12*blockSizeF;
+      const float Cx2_10 = Cx2 + Dx23*blockSizeF;
+      const float Cx3_10 = Cx3 + Dx31*blockSizeF;
+
+      const float Cx1_11 = Cx1 + Dx12*blockSizeF - Dy12*blockSizeF;
+      const float Cx2_11 = Cx2 + Dx23*blockSizeF - Dy23*blockSizeF;
+      const float Cx3_11 = Cx3 + Dx31*blockSizeF - Dy31*blockSizeF;
 
       const bool v0Inside = (Cx1_00 > HALF_SPACE_EPSILON && Cx2_00 > HALF_SPACE_EPSILON && Cx3_00 > HALF_SPACE_EPSILON);
       const bool v1Inside = (Cx1_01 > HALF_SPACE_EPSILON && Cx2_01 > HALF_SPACE_EPSILON && Cx3_01 > HALF_SPACE_EPSILON);
@@ -109,182 +96,37 @@ void RasterizeTriHalfSpace2D(const TriangleLocal& tri, int tileMinX, int tileMin
       if (v0Inside && v1Inside && v2Inside && v3Inside)
       {
         // RenderBlock(x,j,BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
+        for (int y1 = by; y1 < by + blockSize; y1++)
+          for (int x1 = bx; x1 < bx + blockSize; x1++)
             cbuff[frameBuf->w*y1 + x1] = 0x0000FF00;
       }
       else if (v0Inside || v1Inside || v2Inside || v3Inside)
       {
         // RenderPartiallyCoveredBlock(j,x, BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
+        for (int y1 = by; y1 < by + blockSize; y1++)
+          for (int x1 = bx; x1 < bx + blockSize; x1++)
             cbuff[frameBuf->w*y1 + x1] = 0x00FF0000;
-        //break;
       }
 
+
+      Cx1 -= Dy12*blockSizeF;
+      Cx2 -= Dy23*blockSizeF;
+      Cx3 -= Dy31*blockSizeF;
     }
 
-    
-    for (int x = midPoint; x > tri.bb_iminX - BlockSize; x -= BlockSize) // k = 1
-    {
-      const float fx1 = float(x);
-      const float fy1 = float(j);
-      const float fx2 = float(x + BlockSize - 1);
-      const float fy2 = float(j + BlockSize - 1);
+    Cy1 += Dx12*blockSizeF;
+    Cy2 += Dx23*blockSizeF;
+    Cy3 += Dx31*blockSizeF;
 
-      float Cx1_00 = C1 + Dx12 * fy1 - Dy12 * fx1;
-      float Cx2_00 = C2 + Dx23 * fy1 - Dy23 * fx1;
-      float Cx3_00 = C3 + Dx31 * fy1 - Dy31 * fx1;
-
-      float Cx1_01 = C1 + Dx12 * fy1 - Dy12 * fx2;
-      float Cx2_01 = C2 + Dx23 * fy1 - Dy23 * fx2;
-      float Cx3_01 = C3 + Dx31 * fy1 - Dy31 * fx2;
-
-      float Cx1_10 = C1 + Dx12 * fy2 - Dy12 * fx1;
-      float Cx2_10 = C2 + Dx23 * fy2 - Dy23 * fx1;
-      float Cx3_10 = C3 + Dx31 * fy2 - Dy31 * fx1;
-
-      float Cx1_11 = C1 + Dx12 * fy2 - Dy12 * fx2;
-      float Cx2_11 = C2 + Dx23 * fy2 - Dy23 * fx2;
-      float Cx3_11 = C3 + Dx31 * fy2 - Dy31 * fx2;
-
-      const bool v0Inside = (Cx1_00 > HALF_SPACE_EPSILON && Cx2_00 > HALF_SPACE_EPSILON && Cx3_00 > HALF_SPACE_EPSILON);
-      const bool v1Inside = (Cx1_01 > HALF_SPACE_EPSILON && Cx2_01 > HALF_SPACE_EPSILON && Cx3_01 > HALF_SPACE_EPSILON);
-      const bool v2Inside = (Cx1_10 > HALF_SPACE_EPSILON && Cx2_10 > HALF_SPACE_EPSILON && Cx3_10 > HALF_SPACE_EPSILON);
-      const bool v3Inside = (Cx1_11 > HALF_SPACE_EPSILON && Cx2_11 > HALF_SPACE_EPSILON && Cx3_11 > HALF_SPACE_EPSILON);
-
-      if (v0Inside && v1Inside && v2Inside && v3Inside)
-      {
-        // RenderBlock(x,j,BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x0000FF00;
-      }
-      else if (v0Inside || v1Inside || v2Inside || v3Inside)
-      {
-        // RenderPartiallyCoveredBlock(j,x, BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x00FF0000;
-        //break;
-      }
-     
-     
-    }
-
-
+    offset = nextLine(offset, frameBuf->w, frameBuf->h);
   }
-
-  // Find Bottommost vertex, calculate leftPoint and rightPoint
-  // Loop j = maxY &~(BlockSize - 1) to halfY step = -BlockSize
-  //
-  const int startY = tri.bb_imaxY & ~(BlockSize - 1);
-  for (int j = startY; j >= halfY; j -= BlockSize)
-  {
-    const int midPoint = leftPoint + ((rightPoint - leftPoint) >> 1) & ~(BlockSize - 1);
-
-    //Loop k=0 to 2 step=1
-    //Loop x to (q > 0 ? x < maxX : x > minX - BlockSize) step=q
-    for (int x = midPoint; x < tri.bb_imaxX; x += BlockSize) // k = 0
-    {
-      const float fx1 = float(x);
-      const float fy1 = float(j);
-      const float fx2 = float(x + BlockSize - 1);
-      const float fy2 = float(j + BlockSize - 1);
-
-      float Cx1_00 = C1 + Dx12 * fy1 - Dy12 * fx1;
-      float Cx2_00 = C2 + Dx23 * fy1 - Dy23 * fx1;
-      float Cx3_00 = C3 + Dx31 * fy1 - Dy31 * fx1;
-
-      float Cx1_01 = C1 + Dx12 * fy1 - Dy12 * fx2;
-      float Cx2_01 = C2 + Dx23 * fy1 - Dy23 * fx2;
-      float Cx3_01 = C3 + Dx31 * fy1 - Dy31 * fx2;
-
-      float Cx1_10 = C1 + Dx12 * fy2 - Dy12 * fx1;
-      float Cx2_10 = C2 + Dx23 * fy2 - Dy23 * fx1;
-      float Cx3_10 = C3 + Dx31 * fy2 - Dy31 * fx1;
-
-      float Cx1_11 = C1 + Dx12 * fy2 - Dy12 * fx2;
-      float Cx2_11 = C2 + Dx23 * fy2 - Dy23 * fx2;
-      float Cx3_11 = C3 + Dx31 * fy2 - Dy31 * fx2;
-
-      const bool v0Inside = (Cx1_00 > HALF_SPACE_EPSILON && Cx2_00 > HALF_SPACE_EPSILON && Cx3_00 > HALF_SPACE_EPSILON);
-      const bool v1Inside = (Cx1_01 > HALF_SPACE_EPSILON && Cx2_01 > HALF_SPACE_EPSILON && Cx3_01 > HALF_SPACE_EPSILON);
-      const bool v2Inside = (Cx1_10 > HALF_SPACE_EPSILON && Cx2_10 > HALF_SPACE_EPSILON && Cx3_10 > HALF_SPACE_EPSILON);
-      const bool v3Inside = (Cx1_11 > HALF_SPACE_EPSILON && Cx2_11 > HALF_SPACE_EPSILON && Cx3_11 > HALF_SPACE_EPSILON);
-
-      if (v0Inside && v1Inside && v2Inside && v3Inside)
-      {
-        // RenderBlock(x,j,BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x0000FF00;
-      }
-      else if (v0Inside || v1Inside || v2Inside || v3Inside)
-      {
-        // RenderPartiallyCoveredBlock(j,x, BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x00FF0000;
-        //break;
-      }
-
-    }
-
-
-    for (int x = midPoint; x > tri.bb_iminX - BlockSize; x -= BlockSize) // k = 1
-    {
-      const float fx1 = float(x);
-      const float fy1 = float(j);
-      const float fx2 = float(x + BlockSize - 1);
-      const float fy2 = float(j + BlockSize - 1);
-
-      float Cx1_00 = C1 + Dx12 * fy1 - Dy12 * fx1;
-      float Cx2_00 = C2 + Dx23 * fy1 - Dy23 * fx1;
-      float Cx3_00 = C3 + Dx31 * fy1 - Dy31 * fx1;
-
-      float Cx1_01 = C1 + Dx12 * fy1 - Dy12 * fx2;
-      float Cx2_01 = C2 + Dx23 * fy1 - Dy23 * fx2;
-      float Cx3_01 = C3 + Dx31 * fy1 - Dy31 * fx2;
-
-      float Cx1_10 = C1 + Dx12 * fy2 - Dy12 * fx1;
-      float Cx2_10 = C2 + Dx23 * fy2 - Dy23 * fx1;
-      float Cx3_10 = C3 + Dx31 * fy2 - Dy31 * fx1;
-
-      float Cx1_11 = C1 + Dx12 * fy2 - Dy12 * fx2;
-      float Cx2_11 = C2 + Dx23 * fy2 - Dy23 * fx2;
-      float Cx3_11 = C3 + Dx31 * fy2 - Dy31 * fx2;
-
-      const bool v0Inside = (Cx1_00 > HALF_SPACE_EPSILON && Cx2_00 > HALF_SPACE_EPSILON && Cx3_00 > HALF_SPACE_EPSILON);
-      const bool v1Inside = (Cx1_01 > HALF_SPACE_EPSILON && Cx2_01 > HALF_SPACE_EPSILON && Cx3_01 > HALF_SPACE_EPSILON);
-      const bool v2Inside = (Cx1_10 > HALF_SPACE_EPSILON && Cx2_10 > HALF_SPACE_EPSILON && Cx3_10 > HALF_SPACE_EPSILON);
-      const bool v3Inside = (Cx1_11 > HALF_SPACE_EPSILON && Cx2_11 > HALF_SPACE_EPSILON && Cx3_11 > HALF_SPACE_EPSILON);
-
-      if (v0Inside && v1Inside && v2Inside && v3Inside)
-      {
-        // RenderBlock(x,j,BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x0000FF00;
-      }
-      else if (v0Inside || v1Inside || v2Inside || v3Inside)
-      {
-        // RenderPartiallyCoveredBlock(j,x, BlockSize);
-        for (int y1 = j; y1 < j + BlockSize; y1++)
-          for (int x1 = x; x1 < x + BlockSize; x1++)
-            cbuff[frameBuf->w*y1 + x1] = 0x00FF0000;
-        //break;
-      }
-    }
-
-  }
-
 }
+
 
 
 void HWImplBlock4x4::RasterizeTriangle(ROP_TYPE a_ropT, const TriangleType& tri, int tileMinX, int tileMinY,
                                        FrameBuffer* frameBuf)
 {
-  RasterizeTriHalfSpace2D(tri, tileMinX, tileMinY,
-                          frameBuf);
+  RasterizeTriHalfSpace2D_Block(tri, tileMinX, tileMinY,
+                                frameBuf);
 }
