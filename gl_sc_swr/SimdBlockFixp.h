6a0ace6341b1c233184e123ab1df2126a711677f
FROL256 <vova@frolov.pp.ru>
2018-10-28 14:38:42 +0300

    edit

--- gl_sc_swr/SimdBlockFixp.h
+++ gl_sc_swr/SimdBlockFixp.h
@@ -15,18 +15,18 @@ static inline int iround(float f) { return (int)f; }
 
 
 template<typename Triangle, int blockSize>
-void RasterizeTriHalfSpaceBlockFixp(const Triangle& tri, int tileMinX, int tileMinY,
-                                    FrameBuffer* frameBuf)
+void RasterizeTriHalfSpaceBlockFixp2D_Fill(const Triangle& tri, int tileMinX, int tileMinY,
+                                           FrameBuffer* frameBuf)
 {
 
   // 28.4 fixed-point coordinates
-  const int Y1 = iround(16.0f * tri.v3.y) - tileMinY;
-  const int Y2 = iround(16.0f * tri.v2.y) - tileMinY;
-  const int Y3 = iround(16.0f * tri.v1.y) - tileMinY;
+  const int Y1 = iround(16.0f * tri.v3.y) - 16*tileMinY;
+  const int Y2 = iround(16.0f * tri.v2.y) - 16*tileMinY;
+  const int Y3 = iround(16.0f * tri.v1.y) - 16*tileMinY;
 
-  const int X1 = iround(16.0f * tri.v3.x) - tileMinX;
-  const int X2 = iround(16.0f * tri.v2.x) - tileMinX;
-  const int X3 = iround(16.0f * tri.v1.x) - tileMinX;
+  const int X1 = iround(16.0f * tri.v3.x) - 16*tileMinX;
+  const int X2 = iround(16.0f * tri.v2.x) - 16*tileMinX;
+  const int X3 = iround(16.0f * tri.v1.x) - 16*tileMinX;
 
   // Deltas
   const int DX12 = X1 - X2;
@@ -153,5 +153,170 @@ void RasterizeTriHalfSpaceBlockFixp(const Triangle& tri, int tileMinX, int tileM
 
 }
 
+
+template<typename Triangle, int blockSize>
+void RasterizeTriHalfSpaceBlockFixp2D(const Triangle &tri, int tileMinX, int tileMinY,
+                                      FrameBuffer *frameBuf)
+{
+
+  // 28.4 fixed-point coordinates
+  const int Y1 = iround(16.0f * tri.v3.y) - 16*tileMinY;
+  const int Y2 = iround(16.0f * tri.v2.y) - 16*tileMinY;
+  const int Y3 = iround(16.0f * tri.v1.y) - 16*tileMinY;
+
+  const int X1 = iround(16.0f * tri.v3.x) - 16*tileMinX;
+  const int X2 = iround(16.0f * tri.v2.x) - 16*tileMinX;
+  const int X3 = iround(16.0f * tri.v1.x) - 16*tileMinX;
+
+  // Deltas
+  const int DX12 = X1 - X2;
+  const int DX23 = X2 - X3;
+  const int DX31 = X3 - X1;
+
+  const int DY12 = Y1 - Y2;
+  const int DY23 = Y2 - Y3;
+  const int DY31 = Y3 - Y1;
+
+  // Fixed-point deltas
+  const int FDX12 = DX12 << 4;
+  const int FDX23 = DX23 << 4;
+  const int FDX31 = DX31 << 4;
+
+  const int FDY12 = DY12 << 4;
+  const int FDY23 = DY23 << 4;
+  const int FDY31 = DY31 << 4;
+
+  // Bounding rectangle
+  const int minx = ( imax(tri.bb_iminX - tileMinX, 0)               ) & ~(blockSize - 1);  // Start in corner of 8x8 block
+  const int miny = ( imax(tri.bb_iminY - tileMinY, 0)               ) & ~(blockSize - 1);  // Start in corner of 8x8 block
+  const int maxx = ( imin(tri.bb_imaxX - tileMinX, frameBuf->w - 1) );
+  const int maxy = ( imin(tri.bb_imaxY - tileMinY, frameBuf->h - 1) );
+
+  const int pitch  = frameBuf->pitch;
+  int* colorBuffer = frameBuf->cbuffer + miny * pitch;
+
+  // Half-edge constants
+  int C1 = DY12 * X1 - DX12 * Y1;
+  int C2 = DY23 * X2 - DX23 * Y2;
+  int C3 = DY31 * X3 - DX31 * Y3;
+
+  // Correct for fill convention
+  if (DY12 < 0 || (DY12 == 0 && DX12 > 0)) C1++;
+  if (DY23 < 0 || (DY23 == 0 && DX23 > 0)) C2++;
+  if (DY31 < 0 || (DY31 == 0 && DX31 > 0)) C3++;
+
+  const float areaInv = 1.0f / (256.0f*fabs(float( (DY31*DX12 >> 8) - (DX31*DY12 >> 8) )));
+
+  // Loop through blocks
+  for (int y = miny; y < maxy; y += blockSize)
+  {
+    for (int x = minx; x < maxx; x += blockSize)
+    {
+      // Corners of block
+      const int x0 = x << 4;
+      const int x1 = (x + blockSize - 1) << 4;
+      const int y0 = y << 4;
+      const int y1 = (y + blockSize - 1) << 4;
+
+      // Evaluate half-space functions
+      const bool a00 = C1 + DX12 * y0 - DY12 * x0 > 0;
+      const bool a10 = C1 + DX12 * y0 - DY12 * x1 > 0;
+      const bool a01 = C1 + DX12 * y1 - DY12 * x0 > 0;
+      const bool a11 = C1 + DX12 * y1 - DY12 * x1 > 0;
+      const int  a   = (a00 << 0) | (a10 << 1) | (a01 << 2) | (a11 << 3);
+
+      const bool b00 = C2 + DX23 * y0 - DY23 * x0 > 0;
+      const bool b10 = C2 + DX23 * y0 - DY23 * x1 > 0;
+      const bool b01 = C2 + DX23 * y1 - DY23 * x0 > 0;
+      const bool b11 = C2 + DX23 * y1 - DY23 * x1 > 0;
+      const int  b   = (b00 << 0) | (b10 << 1) | (b01 << 2) | (b11 << 3);
+
+      const bool c00 = C3 + DX31 * y0 - DY31 * x0 > 0;
+      const bool c10 = C3 + DX31 * y0 - DY31 * x1 > 0;
+      const bool c01 = C3 + DX31 * y1 - DY31 * x0 > 0;
+      const bool c11 = C3 + DX31 * y1 - DY31 * x1 > 0;
+      const int  c   = (c00 << 0) | (c10 << 1) | (c01 << 2) | (c11 << 3);
+
+      // Skip block when outside an edge
+      if (a == 0x0 || b == 0x0 || c == 0x0)
+        continue;
+
+      int *buffer = colorBuffer;
+
+      // Accept whole block when totally covered
+      if (a == 0xF && b == 0xF && c == 0xF)
+      {
+        int CY1 = C1 + DX12 * y0 - DY12 * x0;
+        int CY2 = C2 + DX23 * y0 - DY23 * x0;
+        int CY3 = C3 + DX31 * y0 - DY31 * x0;
+
+        for (int iy = 0; iy < blockSize; iy++)
+        {
+          int CX1 = CY1;
+          int CX2 = CY2;
+          int CX3 = CY3;
+
+          for (int ix = x; ix < x + blockSize; ix++)
+          {
+            const float w1 = areaInv*float(CX1);
+            const float w2 = areaInv*float(CX3);
+
+            const float4 c = tri.c1*w1 + tri.c2*w2 + tri.c3*(1.0f - w1 - w2);
+            buffer[ix] = RealColorToUint32_BGRA(c);
+
+            CX1 -= FDY12;
+            CX2 -= FDY23;
+            CX3 -= FDY31;
+          }
+
+          CY1 += FDX12;
+          CY2 += FDX23;
+          CY3 += FDX31;
+
+          buffer += pitch;
+        }
+      }
+      else
+      {
+        int CY1 = C1 + DX12 * y0 - DY12 * x0;
+        int CY2 = C2 + DX23 * y0 - DY23 * x0;
+        int CY3 = C3 + DX31 * y0 - DY31 * x0;
+
+        for (int iy = y; iy < y + blockSize; iy++)
+        {
+          int CX1 = CY1;
+          int CX2 = CY2;
+          int CX3 = CY3;
+
+          for (int ix = x; ix < x + blockSize; ix++)
+          {
+            if (CX1 > 0 && CX2 > 0 && CX3 > 0)
+            {
+              const float w1 = areaInv*float(CX1);
+              const float w2 = areaInv*float(CX3);
+
+              const float4 c = tri.c1*w1 + tri.c2*w2 + tri.c3*(1.0f - w1 - w2);
+              buffer[ix]     = RealColorToUint32_BGRA(c);
+            }
+
+            CX1 -= FDY12;
+            CX2 -= FDY23;
+            CX3 -= FDY31;
+          }
+
+          CY1 += FDX12;
+          CY2 += FDX23;
+          CY3 += FDX31;
+
+          buffer += pitch;
+        }
+      }
+    }
+
+    colorBuffer += blockSize * pitch;
+  }
+
+
+}
 
 #endif //TEST_GL_TOP_SIMDBLOCKFIXP_H
