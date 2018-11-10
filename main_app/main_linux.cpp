b4c2c7d519b870cbc82ed9987664f9303a88fc30
FROL256 <vova@frolov.pp.ru>
2018-11-10 22:57:54 +0300

    have some problems with reading alpha from texture .. may be int/uint issue

--- main_app/main_linux.cpp
+++ main_app/main_linux.cpp
@@ -100,6 +100,7 @@ void ExposeFunc()
 
       //demo04_pyramid_and_cube_3d(wa.width, wa.height, 10.0f, 13.0f);
       //demo04_pyramid_and_cube_3d(wa.width, wa.height, angle1, angle2);
+      //demo14_transparent_cube(wa.width, wa.height, 10.0f, 13.0f);
       //demo03_many_small_dynamic_triangles();
 
       //test11_alpha_tex_and_transp();
