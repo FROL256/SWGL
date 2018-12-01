b5487c0dc3cd5e9042a0e55c7026fdab071505cd
FROL-HOME\FROL <vova@frolov.pp.ru>
2018-12-01 14:51:29 +0300

    AVX1 does not have integer instructions !!! I need new CPU!

--- gl_sc_swr/vfloat8_x64.h
+++ gl_sc_swr/vfloat8_x64.h
@@ -13,25 +13,6 @@ namespace cvex
   typedef __m256  vfloat8;
   typedef __m256i vint8;
 
-  #ifdef WIN32 // MVSC does not define operators !!!
-
-  static inline vfloat8 operator+(const vfloat8 a, const vfloat8 b) { return _mm256_add_ps(a, b); }
-  static inline vfloat8 operator-(const vfloat8 a, const vfloat8 b) { return _mm256_sub_ps(a, b); }
-  static inline vfloat8 operator*(const vfloat8 a, const vfloat8 b) { return _mm256_mul_ps(a, b); }
-  static inline vfloat8 operator/(const vfloat8 a, const vfloat8 b) { return _mm256_div_ps(a, b); }
-
-  static inline vfloat8 operator+(const vfloat8 a, const float b) { return _mm256_add_ps(a, _mm256_broadcast_ss(&b)); }
-  static inline vfloat8 operator-(const vfloat8 a, const float b) { return _mm256_sub_ps(a, _mm256_broadcast_ss(&b)); }
-  static inline vfloat8 operator*(const vfloat8 a, const float b) { return _mm256_mul_ps(a, _mm256_broadcast_ss(&b)); }
-  static inline vfloat8 operator/(const vfloat8 a, const float b) { return _mm256_div_ps(a, _mm256_broadcast_ss(&b)); }
-
-  static inline vfloat8 operator+(const float a, const vfloat8 b) { return _mm256_add_ps(_mm256_broadcast_ss(&a), b); }
-  static inline vfloat8 operator-(const float a, const vfloat8 b) { return _mm256_sub_ps(_mm256_broadcast_ss(&a), b); }
-  static inline vfloat8 operator*(const float a, const vfloat8 b) { return _mm256_mul_ps(_mm256_broadcast_ss(&a), b); }
-  static inline vfloat8 operator/(const float a, const vfloat8 b) { return _mm256_div_ps(_mm256_broadcast_ss(&a), b); }
-
-  #endif
-
   static inline void set_ftz() { _MM_SET_ROUNDING_MODE(_MM_ROUND_TOWARD_ZERO);}
 
   static inline void store(float* data, vfloat8 a_val)   { _mm256_store_ps(data, a_val);  }
@@ -56,6 +37,9 @@ namespace cvex
   static inline vint8 make_vint(const int a, const int b, const int c, const int d,
                                 const int e, const int f, const int g, const int h) { return _mm256_set_epi32(h, g, f, e, d, c, b, a); }
 
+  static inline vfloat8 as_vfloat(const vint8 a_val) { return _mm256_castsi256_ps(a_val); }
+  static inline vint8   as_vint(const vfloat8 a_val) { return _mm256_castps_si256(a_val); }
+
   static inline vfloat8 rcp_e(const vfloat8 a) { return _mm256_rcp_ps(a); }
 
   static inline vfloat8 blend(const vfloat8 a, const vfloat8 b, const vint8 mask)
@@ -77,5 +61,49 @@ namespace cvex
   static inline void prefetch(const float* ptr) {  _mm_prefetch((const char*)ptr, _MM_HINT_T0); }
   static inline void prefetch(const int* ptr)   {  _mm_prefetch((const char*)ptr, _MM_HINT_T0); }
 };
+
+
+#ifdef WIN32 // MVSC does not define operators !!!
+
+static inline cvex::vfloat8 operator+(const cvex::vfloat8 a, const cvex::vfloat8 b) { return _mm256_add_ps(a, b); }
+static inline cvex::vfloat8 operator-(const cvex::vfloat8 a, const cvex::vfloat8 b) { return _mm256_sub_ps(a, b); }
+static inline cvex::vfloat8 operator*(const cvex::vfloat8 a, const cvex::vfloat8 b) { return _mm256_mul_ps(a, b); }
+static inline cvex::vfloat8 operator/(const cvex::vfloat8 a, const cvex::vfloat8 b) { return _mm256_div_ps(a, b); }
+
+static inline cvex::vfloat8 operator+(const cvex::vfloat8 a, const float b) { return _mm256_add_ps(a, _mm256_broadcast_ss(&b)); }
+static inline cvex::vfloat8 operator-(const cvex::vfloat8 a, const float b) { return _mm256_sub_ps(a, _mm256_broadcast_ss(&b)); }
+static inline cvex::vfloat8 operator*(const cvex::vfloat8 a, const float b) { return _mm256_mul_ps(a, _mm256_broadcast_ss(&b)); }
+static inline cvex::vfloat8 operator/(const cvex::vfloat8 a, const float b) { return _mm256_div_ps(a, _mm256_broadcast_ss(&b)); }
+
+static inline cvex::vfloat8 operator+(const float a, const cvex::vfloat8 b) { return _mm256_add_ps(_mm256_broadcast_ss(&a), b); }
+static inline cvex::vfloat8 operator-(const float a, const cvex::vfloat8 b) { return _mm256_sub_ps(_mm256_broadcast_ss(&a), b); }
+static inline cvex::vfloat8 operator*(const float a, const cvex::vfloat8 b) { return _mm256_mul_ps(_mm256_broadcast_ss(&a), b); }
+static inline cvex::vfloat8 operator/(const float a, const cvex::vfloat8 b) { return _mm256_div_ps(_mm256_broadcast_ss(&a), b); }
+
+static inline cvex::vint8 operator+(const cvex::vint8 a, const cvex::vint8 b) { return _mm256_add_epi32(a, b); }
+static inline cvex::vint8 operator-(const cvex::vint8 a, const cvex::vint8 b) { return _mm256_sub_epi32(a, b); }
+static inline cvex::vint8 operator*(const cvex::vint8 a, const cvex::vint8 b) { return _mm256_mullo_epi32(a, b); }
+
+static inline cvex::vint8 operator+(const cvex::vint8 a, const int b) { return _mm256_add_epi32  (a, cvex::splat(b)); }
+static inline cvex::vint8 operator-(const cvex::vint8 a, const int b) { return _mm256_sub_epi32  (a, cvex::splat(b)); }
+static inline cvex::vint8 operator*(const cvex::vint8 a, const int b) { return _mm256_mullo_epi32(a, cvex::splat(b)); }
+
+static inline cvex::vint8 operator+(const int a, const cvex::vint8 b) { return _mm256_add_epi32  (cvex::splat(a), b); }
+static inline cvex::vint8 operator-(const int a, const cvex::vint8 b) { return _mm256_sub_epi32  (cvex::splat(a), b); }
+static inline cvex::vint8 operator*(const int a, const cvex::vint8 b) { return _mm256_mullo_epi32(cvex::splat(a), b); }
+
+static inline cvex::vint8 operator<<(const cvex::vint8 a, const int val) { return _mm256_slli_epi32(a, val); }
+static inline cvex::vint8 operator>>(const cvex::vint8 a, const int val) { return _mm256_srli_epi32(a, val); }
+
+static inline cvex::vint8 operator|(const cvex::vint8 a, const cvex::vint8 b) { return _mm256_or_si256(a, b); }
+static inline cvex::vint8 operator&(const cvex::vint8 a, const cvex::vint8 b) { return _mm256_and_si256(a, b); }
+
+static inline cvex::vint8 operator> (const cvex::vfloat8 a, const cvex::vfloat8 b) { return cvex::as_vint(_mm256_cmp_ps(a, b, _CMP_GT_OQ)); }
+static inline cvex::vint8 operator< (const cvex::vfloat8 a, const cvex::vfloat8 b) { return cvex::as_vint(_mm256_cmp_ps(a, b, _CMP_LT_OQ)); }
+static inline cvex::vint8 operator>=(const cvex::vfloat8 a, const cvex::vfloat8 b) { return cvex::as_vint(_mm256_cmp_ps(a, b, _CMP_GE_OQ)); }
+static inline cvex::vint8 operator<=(const cvex::vfloat8 a, const cvex::vfloat8 b) { return cvex::as_vint(_mm256_cmp_ps(a, b, _CMP_GE_OQ)); }
+
+#endif
+
 
 #endif //TEST_GL_TOP_VFLOAT8_H
