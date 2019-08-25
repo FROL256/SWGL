2ece2e110a3427a3253e91df410785db3a1281c9
FROL256 <vova@frolov.pp.ru>
2019-08-25 12:57:12 +0300

    fix everything

--- gl_sc_swr/swgl.h
+++ gl_sc_swr/swgl.h
@@ -497,8 +497,8 @@ inline void swglTriangleSetUp(const SWGL_Context *a_pContext, const Batch *pBatc
 
 }
 
-constexpr float NEAR_CLIP_PLANE  = 1.0f;                    // OpenGL Z axis looks to (0,0,-1)
-constexpr float NEAR_CLIP_PLANE2 = NEAR_CLIP_PLANE*0.99995f; // the trick wich has unknown nature ...
+constexpr float NEAR_CLIP_PLANE  = 0.9995f;                 // OpenGL Z axis looks to (0,0,-1); 0.9995f is essential for some unknown reason ...
+constexpr float NEAR_CLIP_PLANE2 = NEAR_CLIP_PLANE*0.9995f; // the trick wich has unknown nature ...
 
 template<typename ClipTriangleType>
 static inline int swglClipTriangle(const ClipTriangleType& a_inTri, ClipTriangleType outTris[2])
